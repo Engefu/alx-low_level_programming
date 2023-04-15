3dcb97ecc57c91065840c2106426225f2104d2da
Engefu <brilliantengefu@gmail.com>
2023-04-15 14:12:03 -0700

    Check 12 update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -4,10 +4,12 @@
  * @str: string to print
  */
 void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str++);
-	}
-		_putchar('\n');
-}
+  {
+   int i = 0;
+   while (str[i])
+    {
+     _putchar(str[i]);
+     i++;
+    } 
+    _putchar('\n');
+   }
