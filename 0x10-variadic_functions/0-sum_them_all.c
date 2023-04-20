04d15dbe07ed2e99a90aae82219eb92756ef5998
Engefu <brilliantengefu@gmail.com>
2023-04-20 06:00:11 -0700

    function that prints strings, followed by a new line

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,26 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
-
 /**
- * sum_them_all - Returns sum of all parameters.
- * n: No of parameters passed to the function.
- * @...: A variable no  of parameters to sum them all.
- *
- * Return: If n == 0 - 0.
- *        else - the sum of all parameters.
+ * sum_them_all - sum all arguements
+ * @n: number of arguements
+ * Return: 0 is n is 0 or return sum
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i, sum = 0;
+	va_list numbers;
+	int sum;
+	unsigned int i;
 
-	va_start(ap, n);
-
+	if (n == 0)
+		return (0);
+	sum = 0;
+	va_start(numbers, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-
-	va_end(ap);
-
+	{
+		sum += va_arg(numbers, int);
+	}
+	va_end(numbers);
 	return (sum);
 }
