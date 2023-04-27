fddaef47b76a1de35833be645b043bb31d63e757
Engefu <brilliantengefu@gmail.com>
2023-04-27 06:45:32 -0700

    Update 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,16 @@ size_t print_list(const list_t *h)
 {
     size_t count = 0; // initialize count variable to zero
 
-    while (h != NULL) // traverse the linked list
-    {
-        if (h->str == NULL) // check if string is null
-            printf("[%d] (nil)\n", h->len);
-        else
-            printf("[%d] %s\n", h->len, h->str);
-
-        h = h->next; // move to the next node
-        count++; // increment the count variable
-    }
+    while (h)
+ 	{
+ 	if (!h->str)
+ 	printf("[0] (nil)\n");
+ 	else
+ 	printf("[%u] %s\n", h->len, h->str);
+	h = h->next;
+ 	count++;
+ 	}	
+}
 
     return (count); // return the number of nodes
 }
