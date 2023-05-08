e175b1dbe72f9aff4c4da37aac1737cb74bce6e3
Engefu <brilliantengefu@gmail.com>
2023-05-08 07:11:03 -0700

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,36 +2,64 @@
 #define BUFFER_SIZE 1024
 
 /**
- * main - copies the content of a file to another file
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success, 97-100 on failure
+ * error_exit - Prints an error message and exits with a specified code
+ * @code: The exit code
+ * @message: The error message to print
  */
-int main(int argc, char *argv[])
+void error_exit(int code, char *message)
 {
-	int fd_from, fd_to, r, w;
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(code);
+}
+
+/**
+ * cp - Copies the content of one file to another
+ * @file_from: The name of the source file
+ * @file_to: The name of the destination file
+ */
+void cp(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to, r_status, w_status;
 	char buffer[BUFFER_SIZE];
 
-	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]), exit(97);
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from < 0)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to < 0)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, dprintf("Error: Can't read from file %s", file_from));
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error_exit(99, dprintf("Error: Can't write to %s", file_to));
+
+	while ((r_status = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		w = write(fd_to, buffer, r);
-		if (w < 0)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		w_status = write(fd_to, buffer, r_status);
+		if (w_status != r_status)
+			error_exit(99, dprintf("Error: Can't write to %s", file_to));
 	}
-	if (r < 0)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	if (close(fd_from) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-	if (close(fd_to) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+
+	if (r_status == -1)
+		error_exit(98, dprintf("Error: Can't read from file %s", file_from));
+
+	if (close(fd_from) == -1)
+		error_exit(100, dprintf("Error: Can't close fd %d", fd_from));
+
+	if (close(fd_to) == -1)
+		error_exit(100, dprintf("Error: Can't close fd %d", fd_to));
+}
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: 0 on success, otherwise an error code
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+		error_exit(97, "Usage: cp file_from file_to");
+
+	cp(argv[1], argv[2]);
+
 	return (0);
 }
